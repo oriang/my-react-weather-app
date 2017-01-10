84230151993fd65193c1efc57802cda85b14cd29
Orian G <oriango93@gmail.com>
2017-01-10 10:30:40 +0200

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+var Main = require('./components/Main.jsx');
+var Weather = require('./components/Weather.jsx');
+var About = require('./components/About.jsx');
+var Examples = require('./components/Examples.jsx');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <Router history={hashHistory}>
+        <Route path="/" component={Main}>
+          <Route path="about" component={About}/>
+          <Route path="examples" component={Examples}/>
+          <IndexRoute component={Weather}/>
+        </Route>
+      </Router>
+    );
+  }
+});
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+);
