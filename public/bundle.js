89e66cefd40585ffe0b65c4cc848e2a68196323d
Orian G <oriango93@gmail.com>
2017-01-11 14:21:25 +0200

    style main and pages with foundation grid

--- public/bundle.js
+++ public/bundle.js
@@ -27235,11 +27235,14 @@
 	    null,
 	    React.createElement(Nav, null),
 	    React.createElement(
-	      'h2',
-	      null,
-	      'Main Component'
-	    ),
-	    props.children
+	      'div',
+	      { className: 'row' },
+	      React.createElement(
+	        'div',
+	        { className: 'columns medium-6 large-4 small-centered' },
+	        props.children
+	      )
+	    )
 	  );
 	};
 
@@ -29028,19 +29031,44 @@
 
 	var React = __webpack_require__(8);
 
+	var _require = __webpack_require__(185),
+	    Link = _require.Link;
+
 	var Examples = function Examples(props) {
 	  return React.createElement(
 	    'div',
 	    null,
 	    React.createElement(
-	      'h3',
-	      null,
+	      'h1',
+	      { className: 'text-center' },
 	      'Examples'
 	    ),
 	    React.createElement(
 	      'p',
 	      null,
-	      'Welcome to Example page'
+	      'Here are a few example locations to try out:'
+	    ),
+	    React.createElement(
+	      'ol',
+	      null,
+	      React.createElement(
+	        'li',
+	        null,
+	        React.createElement(
+	          Link,
+	          { to: '/?location=Tel Aviv' },
+	          'Tel Aviv'
+	        )
+	      ),
+	      React.createElement(
+	        'li',
+	        null,
+	        React.createElement(
+	          Link,
+	          { to: '/?location=New York' },
+	          'New York'
+	        )
+	      )
 	    )
 	  );
 	};
--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,10 +1,20 @@
 var React = require('react');
 
+var {Link} = require('react-router');
+
 var Examples = (props) =>  {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to Example page</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to="/?location=Tel Aviv">Tel Aviv</Link>
+        </li>
+        <li>
+          <Link to="/?location=New York">New York</Link>
+        </li>
+      </ol>
     </div>
   );
 };
